d123b6cb6916de05f7c92290999db24b23d99aa4
lizhishan2009 <lizhishan2009@163.com>
2026-03-08 14:30:39 +0800

    Simple page

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   images: {
-    unoptimized: true,
+    domains: ['www.notion.so', 'images.unsplash.com'],
   },
-  trailingSlash: true,
+  env: {
+    NOTION_PAGE_ID: process.env.NOTION_PAGE_ID || '2f28cf1fb6ec80a7a49bc7fbab717565',
+    NOTION_TOKEN: process.env.NOTION_TOKEN,
+  },
 };
 
 module.exports = nextConfig;
