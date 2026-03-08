8f07dc9a4d053805bcd47a99ebe805f1c2a0c6d6
lizhishan2009 <lizhishan2009@163.com>
2026-03-08 14:04:11 +0800

    Simplify - remove Providers wrapper

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,5 @@
-import { Providers } from '@/components/Providers';
 import './globals.css';
 
 export default function Layout({ children }) {
-  return (
-    <Providers>
-      {children}
-    </Providers>
-  );
+  return children;
 }
